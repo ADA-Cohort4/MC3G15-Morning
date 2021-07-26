6f7deea7e743f22b39ba97eb31d846f73d13d26b
Maritia <maritia@qoala.id>
2021-07-26 11:52:30 +0700

    Init Project

---
+++ MC3G15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MC3G15
+//
+//  Created by Maritia Pangaribuan on 26/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
