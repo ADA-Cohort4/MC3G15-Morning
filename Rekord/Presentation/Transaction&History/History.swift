926acf6199df38c456ff42444e8be74af5ff928a
Peter Lee <peter.lee230597@gmail.com>
2021-08-09 11:44:30 +0700

    Finish history get all transaction

--- Rekord/Presentation/Transaction&History/History.swift
+++ Rekord/Presentation/Transaction&History/History.swift
@@ -29,7 +29,7 @@ class History : UIViewController, UITableViewDelegate, UITableViewDataSource{
     private let refreshControl = UIRefreshControl()
     //nanti data hasil query masukin sini, kalo filter / search reload view dan restart query
     //URUTAN: 0 = partner name, 1 = trid, 2 = type, 3 = status, 4 = total
-    let transData : [[String]] = [["Sinar Jaya", "TR#1028231", "Customer","Pending Payment", "Rp14,000,000"], ["Epic Corp", "TR#213123", "Supplier", "Pending Payment", "Rp14,000,000"]]
+    var transData : [[String]] = [["Sinar Jaya", "TR#1028231", "Customer","Paid", "Rp14,000,000"], ["Epic Corp", "TR#213123", "Supplier", "Pending Payment", "Rp14,000,000"]]
     //let transData : [[String]] = []
     let customerData : [String] = ["Sinar Jaya", "Epic Corp"]
     let typeData : [String] = ["Customer", "Supplier"]
@@ -51,6 +51,7 @@ class History : UIViewController, UITableViewDelegate, UITableViewDataSource{
         
        
         super.viewDidLoad()
+        queryForHistory()
         historyTable.dataSource = self
         historyTable.delegate = self
         configViews()
@@ -201,6 +202,56 @@ class History : UIViewController, UITableViewDelegate, UITableViewDataSource{
         self.refreshControl.endRefreshing()
         
     }
+    
+    func queryForHistory(){
+        // MARK: -QUERY TRANSACTION FOR DASHBOARD
+        TransactionRepository.shared.getAllTransaction(_idBusiness: UserDefaults.standard.string(forKey: "businessID")!) { resultList, result in
+            for result in resultList{
+                var partnerName = ""
+                var type = ""
+                PartnerRepository.shared.getAllPartner { resultPartnerList, resultString in
+                    for resultPartner in resultPartnerList{
+                        if resultPartner.idPartner == result.idPartner{
+                            print("found partner")
+                            partnerName = resultPartner.name!
+                            type = resultPartner.type!.rawValue
+                        }
+                    }
+                }
+                if result.status?.rawValue == "paid"{
+                    let formatter = NumberFormatter()
+                    formatter.locale = Locale.current
+                    formatter.numberStyle = .currencyAccounting
+                    formatter.currencySymbol = "Rp"
+                    formatter.currencyCode = "ID"
+                    
+//                    ARLabel.text = formatter.string(from: NSNumber(value: ARValue))
+                    let rupiah = formatter.string(from: NSNumber(value: result.totalPrice ?? 0))
+                    let list : [String] = [partnerName, "TR#\(result.idTransaction!)", type.capitalized, (result.status!.rawValue).capitalized, rupiah!, result.dueDate!]
+                
+                var dashboardQueryHasSameID : Bool = false
+                
+                for transaction in self.transData{
+                    if list[1] == transaction[1]{
+                        dashboardQueryHasSameID = true
+                    }
+                }
+                
+                if dashboardQueryHasSameID == false{
+                    self.transData.append(list)
+                    
+                }
+                    
+                }
+            }
+        }
+    }
+    
+    
+
+    
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.destination is TransactionDetails{
             let vc = segue.destination as? TransactionDetails
@@ -208,5 +259,15 @@ class History : UIViewController, UITableViewDelegate, UITableViewDataSource{
         }
     }
     
+}
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+      return prefix(1).uppercased() + self.lowercased().dropFirst()
+    }
+
+    mutating func capitalizeFirstLetter() {
+      self = self.capitalizingFirstLetter()
+    }
 }
 
