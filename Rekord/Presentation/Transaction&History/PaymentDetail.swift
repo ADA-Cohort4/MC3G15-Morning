f7156457a1f11f55d795e0f048a8793d950c296b
Peter Lee <peter.lee230597@gmail.com>
2021-08-31 12:25:41 +0700

    Fix bug

--- Rekord/Presentation/Transaction&History/PaymentDetail.swift
+++ Rekord/Presentation/Transaction&History/PaymentDetail.swift
@@ -115,7 +115,7 @@ class PaymentDetail: UIViewController {
     }
     
     @IBAction func shareButtonPressed(_ sender: Any) {
-        let urlWhats = "https://wa.me/\(phone)/?text=Hello \(partnerName.text)!, I have performed the payment on \(paymentDate.text) for total amount \(paidAmount.text) with invoice number \(paymentID)"
+        let urlWhats = "https://wa.me/\(phone)/?text=Hello \(partnerName.text ?? "partner")!, I have performed the payment on \(paymentDate.text ?? "payment date") for total amount \(paidAmount.text ?? "paid amount") with invoice number \(paymentID)"
         if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) {
           if let whatsappURL = NSURL(string: urlString) {
             if UIApplication.shared.canOpenURL(whatsappURL as URL) {
